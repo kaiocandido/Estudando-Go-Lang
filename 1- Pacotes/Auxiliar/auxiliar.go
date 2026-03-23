4c3c397ee5e40a6eb80728fb1b859b59f16b7668
adm <kaio.candf@gmail.com>
2026-03-23 12:04:57 -0300

    estudando sobre pacotes em GO

---
+++ 1- Pacotes/Auxiliar/auxiliar.go
@@ -0,0 +1,11 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+// Funcao para escrever uma frase!!
+func Escrever() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+	imprimirMensagem()
+}
---
+++ 1- Pacotes/Auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+//Funcao para escrever uma frase!!
+func imprimirMensagem() {
+	fmt.Println("Escrevendo texto 2")
+}
